60e93855983f64ab484388d606f58a706724b9ca
futagoya <firdauswnn@gmail.com>
2022-08-26 22:53:39 +0700

    initial commit

---
+++ perulangan-bacabuku.py
@@ -0,0 +1,14 @@
+"""
+Perulangan
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+
+jumlah_buku_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca adalah {jumlah_buku_dibaca}")
+
+for jumlah_buku_dibaca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca adalah {jumlah_buku_dibaca}")
