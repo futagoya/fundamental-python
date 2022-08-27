850caaf711f5763bb54bba9ab40340beb92fa83a
futagoya <firdauswnn@gmail.com>
2022-08-27 17:21:16 +0700

    menggunakan list comprehension

--- sintaksis_list_comprehension.py
+++ sintaksis_list_comprehension.py
@@ -23,6 +23,23 @@ del daftar_buku[:]  # Start:Stop:Step
 for i in range(0, len(daftar_buku_baru)):
     print(daftar_buku_baru[i])
 
+print('\nMembuat list baru: genap')
+daftar_buku = ['1 Naruto', '2 One Piece', '3 Batman', '4 Superman']
+daftar_buku_baru = daftar_buku [1::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru: ganjil')
+daftar_buku = ['1 Naruto', '2 One Piece', '3 Batman', '4 Superman']
+daftar_buku_baru = daftar_buku [0::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru: ujung dibuang')
+daftar_buku = ['1 Naruto', '2 One Piece', '3 Batman', '4 Superman']
+daftar_buku_baru = daftar_buku [0:-1]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
 
 
 
