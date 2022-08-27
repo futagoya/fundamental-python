a63f0001ddea3913901f6deaaa437eef6789dd2f
futagoya <firdauswnn@gmail.com>
2022-08-27 16:47:48 +0700

    menggunakan list

--- sintaksis_list.py
+++ sintaksis_list.py
@@ -16,10 +16,34 @@ print(daftar_buku[0])
 print(daftar_buku[3])
 
 print('\nMenampilkan menggunakan for in range')
-for i in range (0, len(daftar_buku)):
+for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nMenambah 1 buku')
 daftar_buku.append('Sains Kelas 6')
-for i in range (0, len(daftar_buku)):
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n Clear list')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen buku')
+daftar_buku = ['Naruto', 'One Piece', 'Batman', 'Superman']
+daftar_buku[0] = 'Naruto 2'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nHilangkan buku kedua')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nMunculkan buku yang hilang')
+print(buku)
+
+print('\nPop')
+daftar_buku.pop()
+for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
